9af1d07004a7707971cd464fd23358f3ea02188d
sree.cheekatimarla <sree.cheekatimarla@skillnetinc.com>
2022-10-04 10:20:38 -0400

    created activities to jump through screens.

--- app/src/main/java/com/example/proffind/LoginPage.java
+++ app/src/main/java/com/example/proffind/LoginPage.java
@@ -2,15 +2,29 @@ package com.example.proffind;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class LoginPage extends AppCompatActivity {
 
+    TextView signUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
-
         // Here we write all the logic for login page like email and password validation.
+
+        signUp = (TextView) findViewById(R.id.signUpButton);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginPage.this,SignUp.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/proffind/SignUp.java
+++ app/src/main/java/com/example/proffind/SignUp.java
@@ -2,13 +2,29 @@ package com.example.proffind;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class SignUp extends AppCompatActivity {
+
+    ImageView backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        backButton = (ImageView) findViewById(R.id.backToLogin);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignUp.this,LoginPage.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
