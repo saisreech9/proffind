77d362bdf3eff66e1f5a8b3fa5afc7093d4838eb
shanthan0196 <bsaishanthan@gmail.com>
2022-11-08 11:16:41 -0500

    Added relative layout for the bottom navigation menu. created menu items.

--- app/src/main/java/com/example/proffind/HomeFragment.java
+++ app/src/main/java/com/example/proffind/HomeFragment.java
@@ -47,8 +47,6 @@ public class HomeFragment extends Fragment {
 
     }
 
-//    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
